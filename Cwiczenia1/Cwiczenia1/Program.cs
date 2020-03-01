52474d32e46852dcb53b7710bf976cb25c4d2784
s16478 <s16478@pja.edu.pl>
2020-03-01 14:10:46 +0100

    Wrote some code

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using System; // importowanie
+using System.Net.Http;
+using System.Threading.Tasks;
 
-namespace Cwiczenia1
+namespace Cwiczenia1  // w Javie package
+    // strin małą literą
+    // metody wielkimi literami
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var client = new HttpClient();
+            HttpResponseMessage result = await client.GetAsync("https://www.pja.edu.pl");
+
+            if (result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+            }
+
+          
+            
+
+            // JS promise async/wait
+            //Java Future
+            //C# Task async/await
+
+
+            Console.WriteLine("Koniec programu!");
         }
     }
 }
